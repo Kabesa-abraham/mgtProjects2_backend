f0631383d8f2c0a8c3f238aa1faf41c22679cdb4
Kabesa-abraham <yebulaabraham@gmail.com>
2025-05-07 22:23:04 +0100

    add some code in app.ts

--- src/app.ts
+++ src/app.ts
@@ -17,10 +17,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ['http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
 }))
 
 app.use('/backend/auth', useRoutes);
